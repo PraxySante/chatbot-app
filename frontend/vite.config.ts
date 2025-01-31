781c88d94fe51d40c850732d329abce98ac2a0b3
Frederic Oudenot <f.oudenot@gmail.com>
2025-01-31 15:50:29 +0100

    Update vite.config.ts - fix allowedHosts missing coma

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react(),],
   server: {
-    allowedHosts:['chatbotfoch.praxysante.fr']
+    allowedHosts:['chatbotfoch.praxysante.fr'],
     host: true,
     port: 5000,
   },
