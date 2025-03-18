0ca3db39043cc91dc191466052180eb97238484e
Frédéric Oudenot <f.oudenot@gmail.com>
2025-03-18 19:53:22 +0100

    update: docker-compose | update: Dockerfile front | update: Dockerfile: back | update: vite.config.ts - dynamic port | update: main - include suspense and remove error | update: RoutesProvider - remove lazy to component

--- frontend/src/hooks/RoutesProvider.tsx
+++ frontend/src/hooks/RoutesProvider.tsx
@@ -1,7 +1,5 @@
-import { lazy } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const DashBoard = lazy(() => import('../pages/DashBoard'));
+import DashBoard from '../pages/DashBoard';
 
 export default function RoutesProvider() {
   const router = createBrowserRouter([
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   server: {
-    allowedHosts:['chatbotfoch.praxysante.fr'],
+    allowedHosts: [`${process.env.VITE_HOST}`],
     host: true,
-    port: 5000,
+    port: Number(process.env.VITE_PORT), // Utilise le port défini dans .env
   },
   preview: {
-    port: 5000,
+    port: Number(process.env.VITE_PORT), // Idem pour le port de preview
   },
   build: {
-    chunkSizeWarningLimit: 1000, // par exemple, augmenter la limite à 1 Mo
+    chunkSizeWarningLimit: 1000, // Par exemple, augmenter la limite à 1 Mo
   }
 });
