9725e5aea32ab547817ac9324e5a3f9fc1ec0bfb
Frédéric Oudenot <f.oudenot@gmail.com>
2025-06-05 14:50:41 +0200

    update TranscriptionContext - including ws HPSJ | update: axiosAuthSecret - including different baseUrl according domain and project chatbot

--- frontend/src/services/axiosConfiguration/axiosAuthSecret.service.ts
+++ frontend/src/services/axiosConfiguration/axiosAuthSecret.service.ts
@@ -4,28 +4,34 @@ const userLanguage = navigator.language.slice(0, 2);
 const hostname = document.location.hostname;
 
 let project: string = '';
+let baseUrl: string = '';
 
 switch (true) {
   case hostname.includes(import.meta.env.VITE_HOST_FOCH):
     project = import.meta.env.VITE_PROJECT_FOCH;
+    baseUrl = import.meta.env.VITE_URL_API_FRONT_CHATBOT_FOCH
     break;
   case hostname.includes(import.meta.env.VITE_HOST_RECO_CARDIO):
     project = import.meta.env.VITE_PROJECT_ESC;
+    baseUrl = import.meta.env.VITE_URL_API_FRONT_CHATBOT_ESC
     break;
   case hostname.includes(import.meta.env.VITE_HOST_AHP):
     project = import.meta.env.VITE_PROJECT_AHP;
+    baseUrl = import.meta.env.VITE_URL_API_FRONT_CHATBOT_AHP
     break;
-  case hostname.includes(import.meta.env.VITE_HOST_HSPJ):
-    project = import.meta.env.VITE_PROJECT_HSPJ;
+  case hostname.includes(import.meta.env.VITE_HOST_HPSJ):
+    project = import.meta.env.VITE_PROJECT_HPSJ;
+    baseUrl = import.meta.env.VITE_URL_API_FRONT_CHATBOT_HPSJ
     break;
   default:
     project = import.meta.env.VITE_PROJECT_DEV;
+    baseUrl = import.meta.env.VITE_URL_API_FRONT_CHATBOT_DEV
     break;
 }
 
 const axiosAuthSecret = axios.create({
   // Configuration axios to connect to Auth0
-  baseURL: import.meta.env.VITE_URL_API_FRONT_CHATBOT,
+  baseURL: baseUrl,
   headers: { 'content-type': 'application/json' },
 });
 
