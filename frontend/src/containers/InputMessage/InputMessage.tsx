26a134f802b157a13e9630132f31324e7118e682
Frédéric Oudenot <f.oudenot@gmail.com>
2025-12-22 10:09:09 +0100

    update: Header - adding logic | update: InputMessage - comment function not used

--- frontend/src/containers/InputMessage/InputMessage.tsx
+++ frontend/src/containers/InputMessage/InputMessage.tsx
@@ -78,18 +78,18 @@ export default function InputMessage() {
   }
 
   // Function preparing message to ChatBot
-  async function sendMessage(e: any): Promise<void> {
-    e.preventDefault();
-    // Record message written by user
-    if (userContent !== '') {
-      setUserContent('');
-      whoIsWritten(ROLE_NONE);
-      whoIsWritten(ROLE_ASSISTANT);
-      await stockMessageUser(userContent);
-      whoIsWritten(ROLE_NONE);
-      //await loadingMessageContext(userContent);
-    }
-  }
+  // async function sendMessage(e: any): Promise<void> {
+  //   e.preventDefault();
+  //   // Record message written by user
+  //   if (userContent !== '') {
+  //     setUserContent('');
+  //     whoIsWritten(ROLE_NONE);
+  //     whoIsWritten(ROLE_ASSISTANT);
+  //     await stockMessageUser(userContent);
+  //     whoIsWritten(ROLE_NONE);
+  //     //await loadingMessageContext(userContent);
+  //   }
+  // }
 
   // async function loadingMessageContext(userContent: string) {
   //   await stockMessageUser(userContent);
